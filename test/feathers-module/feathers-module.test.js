89da2eb91f707e36347a916b5292a5ef5df65fb3
Marshall Thompson <marshall@creativeideal.net>
2017-03-05 06:49:08 -0700

    Add a test for changing the feathers module name

--- test/feathers-module/feathers-module.test.js
+++ test/feathers-module/feathers-module.test.js
@@ -4,17 +4,31 @@ import makeStore from '../fixtures/store'
 import { makeFeathersRestClient } from '../fixtures/feathers-client'
 
 describe('Feathers Module', () => {
-  it('The feathers module gets added to the store', () => {
-    const store = makeStore()
-    makeFeathersRestClient().configure(feathersVuex(store))
-    assert.deepEqual({services: {}}, store.state.feathers)
+  describe('Configuration', () => {
+    it('allows customizing the feathers module name', () => {
+      const store = makeStore()
+      const feathersModuleName = 'Feathaz'
+      const feathersClient = makeFeathersRestClient()
+        .configure(feathersVuex(store, { feathersModuleName }))
+      const service = feathersClient.service('todos')
+      const options = service.vuexOptions.global
+      assert(options.feathersModuleName === feathersModuleName)
+    })
   })
 
-  it('populates with new services', () => {
-    const store = makeStore()
-    const feathersClient = makeFeathersRestClient()
-      .configure(feathersVuex(store))
-    var todoService = feathersClient.service('api/todos')
-    assert(store.state.feathers.services['api/todos'] === todoService)
+  describe('Basics', () => {
+    it('The feathers module gets added to the store', () => {
+      const store = makeStore()
+      makeFeathersRestClient().configure(feathersVuex(store))
+      assert.deepEqual({services: {}}, store.state.feathers)
+    })
+
+    it('populates with new services', () => {
+      const store = makeStore()
+      const feathersClient = makeFeathersRestClient()
+        .configure(feathersVuex(store))
+      var todoService = feathersClient.service('api/todos')
+      assert(store.state.feathers.services['api/todos'] === todoService)
+    })
   })
 })
