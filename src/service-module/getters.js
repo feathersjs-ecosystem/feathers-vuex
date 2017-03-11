e80185b9a7037d4c20a8d37b8e1989f5a32ae191
Marshall Thompson <marshall@creativeideal.net>
2017-03-11 07:58:30 -0700

    Added getList getter so you can query the store

--- src/service-module/getters.js
+++ src/service-module/getters.js
@@ -1,24 +1,40 @@
 import deepAssign from 'deep-assign'
+import getFilter from 'feathers-query-filters'
+// import { sorter, matcher, select, _ } from 'feathers-commons'
+import { sorter, matcher, _ } from 'feathers-commons'
 
 export default function makeServiceGetters (service) {
   return {
     list (state) {
       return state.ids.map(id => state.keyedById[id])
     },
-    findList: (state, getters) => (params) => {
-      const { query } = params
-      let list = getters.list
-      if (query) {
-        return list.filter(item => {
-          return Object.keys(query).reduce((acc, queryParam) => {
-            if (acc) {
-              return item[queryParam] === query[queryParam]
-            }
-            return false
-          }, true)
-        })
-      } else {
-        return list
+    getList: (state, getters) => (params = {}) => {
+      const { query, filters } = getFilter(params.query || {})
+      let values = _.values(getters.list).filter(matcher(query))
+
+      const total = values.length
+
+      if (filters.$sort) {
+        values.sort(sorter(filters.$sort))
+      }
+
+      if (filters.$skip) {
+        values = values.slice(filters.$skip)
+      }
+
+      if (typeof filters.$limit !== 'undefined') {
+        values = values.slice(0, filters.$limit)
+      }
+
+      if (filters.$select) {
+        values = values.map(value => _.pick(value, ...filters.$select))
+      }
+
+      return {
+        total,
+        limit: filters.$limit,
+        skip: filters.$skip || 0,
+        data: values
       }
     },
     current (state) {
