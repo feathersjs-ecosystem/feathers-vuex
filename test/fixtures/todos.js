7b4baeb281119b2a000942436886fcc4b5854e57
Marshall Thompson <marshall@creativeideal.net>
2017-06-26 22:41:41 -0600

    Add todos fixture data for memory stores

---
+++ test/fixtures/todos.js
@@ -0,0 +1,5 @@
+module.exports = {
+  1: { _id: 1, description: 'Dishes', isComplete: true },
+  2: { _id: 2, description: 'Laundry', isComplete: true },
+  3: { _id: 3, description: 'Groceries', isComplete: true }
+}
