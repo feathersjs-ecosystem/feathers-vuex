ba97d10d526c9d742fcb0ef9c13ac470dcb94e23
Marshall Thompson <marshall@creativeideal.net>
2018-10-14 22:53:50 -0600

    Remove unused function

--- src/FeathersVuexGet.js
+++ src/FeathersVuexGet.js
@@ -125,14 +125,3 @@ export default {
     return this.$scopedSlots.default(this.scope)
   }
 }
-
-function randomString (length) {
-  let text = ''
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length))
-  }
-
-  return text
-}
