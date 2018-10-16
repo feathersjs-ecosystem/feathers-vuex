f30d4e73b9a7816cd8c579dd4a748c27eb61faf4
Marshall Thompson <marshall@creativeideal.net>
2018-10-16 03:17:13 -0600

    makeFindMixin at feature parity with component

--- src/make-find-mixin.js
+++ src/make-find-mixin.js
@@ -1,42 +1,41 @@
 import inflection from 'inflection'
 
 export default function makeFindMixin (options) {
-  const { service, name, query, fetchQuery, queryWhen = true, local = false } = options
+  const { service, name, query, fetchQuery, queryWhen = true, local = false, qid = 'default' } = options
   let { watch = [] } = options
   if (typeof watch === 'string') {
     watch = [watch]
+  } else if (typeof watch === 'boolean' && watch) {
+    watch = ['query']
   }
 
   if (!service || typeof service !== 'string') {
     throw new Error(`The 'service' option is required in the FeathersVuex make-find-mixin and must be a string.`)
   }
-
-  const prefix = name || inflection.camelize(service, true)
-  const capitalized = inflection.capitalize(prefix)
+  const nameToUse = name || service
+  const prefix = inflection.camelize(nameToUse, true)
+  const capitalized = prefix.charAt(0).toUpperCase() + prefix.slice(1)
   const ITEMS = prefix
   const IS_FIND_PENDING = `isFind${capitalized}Pending`
   const QUERY = `${prefix}Query`
   const FETCH_QUERY = `${prefix}FetchQuery`
+  const WATCH = `${prefix}Watch`
   const QUERY_WHEN = `${prefix}QueryWhen`
   const FIND_ACTION = `find${capitalized}`
+  const PAGINATION = `${prefix}PaginationData`
   const LOCAL = `${prefix}Local`
+  const QID = `${prefix}Qid`
+  const data = {
+    [IS_FIND_PENDING]: false,
+    [WATCH]: watch,
+    [QID]: qid
+  }
 
   const mixin = {
     data () {
-      return {
-        [IS_FIND_PENDING]: false
-      }
+      return data
     },
     computed: {
-      [QUERY]: typeof query === 'string'
-        ? function () {
-          // If the specified computed prop wasn't found, display an error.
-          if (!Object.getPrototypeOf(this).hasOwnProperty(query)) {
-            throw new Error(`Query was not found at ${query}.`)
-          }
-          return this[query]
-        }
-        : query,
       [ITEMS] () {
         return this[QUERY] ? this.$store.getters[`${service}/find`]({ query: this[QUERY] }).data : []
       }
@@ -50,7 +49,13 @@ export default function makeFindMixin (options) {
             this[IS_FIND_PENDING] = true
 
             if (queryToUse) {
-              return this.$store.dispatch(`${service}/find`, { query: queryToUse })
+              const params = { query: queryToUse }
+
+              if (qid) {
+                params.qid = qid
+              }
+
+              return this.$store.dispatch(`${service}/find`, params)
                 .then(() => {
                   this[IS_FIND_PENDING] = false
                 })
@@ -80,24 +85,30 @@ export default function makeFindMixin (options) {
     }
   }
 
-  // // Add the fetchQuery, if provided
-  if (fetchQuery) {
-    mixin.computed[FETCH_QUERY] = typeof fetchQuery === 'string' ? function () { return this[fetchQuery] } : fetchQuery
+  if (qid) {
+    mixin.computed[PAGINATION] = function () {
+      return this.$store.state[service].pagination[qid]
+    }
   }
+  setupAttribute(QUERY, query)
+  setupAttribute(FETCH_QUERY, fetchQuery)
+  setupAttribute(QUERY_WHEN, queryWhen, 'method')
+  setupAttribute(LOCAL, local)
 
-  // // Add the queryWhen, if provided
-  if (queryWhen) {
-    mixin.computed[QUERY_WHEN] = typeof queryWhen === 'string'
-      ? function () { return this[queryWhen] }
-      : function () { return queryWhen }
-  }
-
-  if (typeof local === 'boolean') {
-    mixin.data[LOCAL] = !!local
-  }
-  // TODO: make sure this works
-  if (typeof local === 'function') {
-    mixin.data[LOCAL] = local
+  function setupAttribute (NAME, value, computedOrMethod = 'computed') {
+    if (typeof value === 'boolean') {
+      data[NAME] = !!value
+    } else if (typeof value === 'string') {
+      mixin.computed[NAME] = function () {
+        // If the specified computed prop wasn't found, display an error.
+        if (!Object.getPrototypeOf(this).hasOwnProperty(value)) {
+          throw new Error(`Value for ${NAME} was not found on the component at '${value}'.`)
+        }
+        return this[value]
+      }
+    } else if (typeof value === 'function') {
+      mixin[computedOrMethod][NAME] = value
+    }
   }
 
   return mixin
