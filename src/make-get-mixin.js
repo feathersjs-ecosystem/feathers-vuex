f41ad19f459cf6aedad16968ad6412f060185772
Marshall Thompson <marshall@creativeideal.net>
2018-10-21 23:25:03 -0600

    Update makeTetMixin.js

--- src/make-get-mixin.js
+++ src/make-get-mixin.js
@@ -1,22 +1,31 @@
 import inflection from 'inflection'
 
 export default function makeFindMixin (options) {
-  const { service, name, query, fetchQuery, id, queryWhen = true, local = false, qid = 'default' } = options
-  let { watch = [] } = options
+  const { service, query, fetchQuery, id, queryWhen = true, local = false, qid = 'default', item } = options
+  let { name, watch = [] } = options
+
   if (typeof watch === 'string') {
     watch = [watch]
   } else if (typeof watch === 'boolean' && watch) {
     watch = ['query']
   }
 
-  if (!service || typeof service !== 'string') {
+  if (!service || (typeof service !== 'string' && typeof service !== 'function')) {
     throw new Error(`The 'service' option is required in the FeathersVuex make-find-mixin and must be a string.`)
   }
-  const nameToUse = name || service
+  if (typeof service === 'function' && !name) {
+    name = 'service'
+  }
+
+  const nameToUse = (name || service).replace('-', '_')
   const singularized = inflection.singularize(nameToUse)
   const prefix = inflection.camelize(singularized, true)
   const capitalized = prefix.charAt(0).toUpperCase() + prefix.slice(1)
-  const ITEM = prefix
+  const SERVICE_NAME = `${prefix}ServiceName`
+  let ITEM = item || prefix
+  if (typeof service === 'function' && name === 'service' && !item) {
+    ITEM = 'item'
+  }
   const IS_GET_PENDING = `isGet${capitalized}Pending`
   const QUERY = `${prefix}Query`
   const FETCH_QUERY = `${prefix}FetchQuery`
@@ -38,7 +47,7 @@ export default function makeFindMixin (options) {
     },
     computed: {
       [ITEM] () {
-        return this[QUERY] ? this.$store.getters[`${service}/find`]({ query: this[QUERY] }).data : []
+        return this[QUERY] ? this.$store.getters[`${SERVICE_NAME}/find`]({ query: this[QUERY] }).data : []
       }
     },
     methods: {
@@ -52,7 +61,7 @@ export default function makeFindMixin (options) {
             if (this[ID]) {
               const params = { query: queryToUse }
 
-              return this.$store.dispatch(`${service}/get`, params)
+              return this.$store.dispatch(`${SERVICE_NAME}/get`, params)
                 .then(() => {
                   this[IS_GET_PENDING] = false
                 })
@@ -62,7 +71,7 @@ export default function makeFindMixin (options) {
       }
     },
     created () {
-      if (this[QUERY] || this[FETCH_QUERY]) {
+      if (this[ID] || this[QUERY] || this[FETCH_QUERY]) {
         watch.forEach(prop => {
           if (typeof prop !== 'string') {
             throw new Error(`Values in the 'watch' array must be strings.`)
@@ -82,22 +91,23 @@ export default function makeFindMixin (options) {
     }
   }
 
+  setupAttribute(SERVICE_NAME, service, 'computed', true)
   setupAttribute(ID, id)
   setupAttribute(QUERY, query)
   setupAttribute(FETCH_QUERY, fetchQuery)
   setupAttribute(QUERY_WHEN, queryWhen, 'method')
   setupAttribute(LOCAL, local)
 
-  function setupAttribute (NAME, value, computedOrMethod = 'computed') {
+  function setupAttribute (NAME, value, computedOrMethod = 'computed', returnTheValue = false) {
     if (typeof value === 'boolean') {
       data[NAME] = !!value
     } else if (typeof value === 'string') {
       mixin.computed[NAME] = function () {
         // If the specified computed prop wasn't found, display an error.
-        if (!Object.getPrototypeOf(this).hasOwnProperty(value)) {
+        if (!Object.getPrototypeOf(this).hasOwnProperty(value) && !Object.getPrototypeOf(this).hasOwnProperty(NAME)) {
           throw new Error(`Value for ${NAME} was not found on the component at '${value}'.`)
         }
-        return this[value]
+        return returnTheValue ? value : this[value]
       }
     } else if (typeof value === 'function') {
       mixin[computedOrMethod][NAME] = value
