e4c0de8a359302b2755e742e7ca025ad987d9ae4
Ray Foss <ray@vblip.com>
2018-11-16 09:45:46 -0600

    auth-module: modulirize reponse handler to allow offline mocking

--- src/auth-module/actions.js
+++ src/auth-module/actions.js
@@ -1,44 +1,46 @@
 export default function makeAuthActions (feathersClient, globalModels) {
   return {
-    authenticate (store, data) {
-      const { commit, state, dispatch } = store
-
+    authenticate ({ commit, state, dispatch }, data) {
       commit('setAuthenticatePending')
       if (state.errorOnAuthenticate) {
         commit('clearAuthenticateError')
       }
       return feathersClient.authenticate(data)
         .then(response => {
-          if (response.accessToken) {
-            commit('setAccessToken', response.accessToken)
-
-            // Decode the token and set the payload, but return the response
-            return feathersClient.passport.verifyJWT(response.accessToken)
-              .then(payload => {
-                commit('setPayload', payload)
-
-                // Populate the user if the userService was provided
-                if (state.userService && payload.hasOwnProperty('userId')) {
-                  return dispatch('populateUser', payload.userId)
-                    .then(() => {
-                      commit('unsetAuthenticatePending')
-                      return response
-                    })
-                } else {
-                  commit('unsetAuthenticatePending')
-                }
-                return response
-              })
-          // If there was not an accessToken in the response, allow the response to pass through to handle two-factor-auth
-          } else {
-            return response
-          }
+          return dispatch('responseHandler', response)
         })
         .catch(error => {
           commit('setAuthenticateError', error)
           commit('unsetAuthenticatePending')
           return Promise.reject(error)
         })
+    },
+    
+    responseHandler ({ commit, state, dispatch }, response) {
+      if (response.accessToken) {
+        commit('setAccessToken', response.accessToken)
+
+        // Decode the token and set the payload, but return the response
+        return feathersClient.passport.verifyJWT(response.accessToken)
+          .then(payload => {
+            commit('setPayload', payload)
+
+            // Populate the user if the userService was provided
+            if (state.userService && payload.hasOwnProperty('userId')) {
+              return dispatch('populateUser', payload.userId)
+                .then(() => {
+                  commit('unsetAuthenticatePending')
+                  return response
+                })
+            } else {
+              commit('unsetAuthenticatePending')
+            }
+            return response
+          })
+      // If there was not an accessToken in the response, allow the response to pass through to handle two-factor-auth
+      } else {
+        return response
+      }
     },
 
     populateUser ({ commit, state, rootState, dispatch }, userId) {
