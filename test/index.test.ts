006d32f1ff04198c10eb158e38e1e99ed8483dc5
Marshall Thompson <marshall@creativeideal.net>
2019-03-26 22:46:37 -0600

    Add test for exports

---
+++ test/index.test.ts
@@ -0,0 +1,40 @@
+import { assert } from 'chai'
+import * as feathersVuex from '../src/index'
+import { feathersSocketioClient as feathersClient } from './fixtures/feathers-client'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+describe('feathers-vuex', () => {
+  it('has correct exports', () => {
+    assert(typeof feathersVuex.default === 'function')
+    assert(
+      typeof feathersVuex.FeathersVuex.install === 'function',
+      'has Vue Plugin'
+    )
+    assert(feathersVuex.FeathersVuexFind)
+    assert(feathersVuex.FeathersVuexGet)
+    assert(feathersVuex.initAuth)
+    assert(feathersVuex.makeFindMixin)
+    assert(feathersVuex.makeGetMixin)
+    assert(feathersVuex.models)
+  })
+
+  it('requires a Feathers Client instance', () => {
+    try {
+      console.log(Object.keys(feathersVuex))
+      feathersVuex.default(
+        {},
+        {
+          serverAlias: 'index-test'
+        }
+      )
+    } catch (error) {
+      assert(
+        error.message ===
+          'The first argument to feathersVuex must be a feathers client.'
+      )
+    }
+  })
+})
