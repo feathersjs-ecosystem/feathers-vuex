d343b116f4a7a769c91af71ad63211b35d84b243
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-03-23 22:50:59 +0100

    add types to service

--- src/service-module/make-service-module.ts
+++ src/service-module/make-service-module.ts
@@ -7,16 +7,21 @@ import makeDefaultState from './service-module.state'
 import makeGetters from './service-module.getters'
 import makeMutations from './service-module.mutations'
 import makeActions from './service-module.actions'
+import { Service } from '@feathersjs/feathers'
+import { MakeServicePluginOptions } from './types'
 
-export default function makeServiceModule(service, options) {
-  const defaultState = makeDefaultState(options.servicePath, options)
+export default function makeServiceModule(
+  service: Service<any>,
+  options: MakeServicePluginOptions
+) {
+  const defaultState = makeDefaultState(options)
   const defaultGetters = makeGetters()
   const defaultMutations = makeMutations()
   const defaultActions = makeActions(service)
 
   return {
     namespaced: true,
-    state: Object.assign(defaultState, options.state),
+    state: Object.assign({}, defaultState, options.state),
     getters: Object.assign({}, defaultGetters, options.getters),
     mutations: Object.assign({}, defaultMutations, options.mutations),
     actions: Object.assign({}, defaultActions, options.actions)
