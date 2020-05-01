c4071276ffc8130342b52a6f5091b3181adb49a0
Alex Hamilton <hamilton.alex7@gmail.com>
2020-05-02 10:18:27 +1200

    new: allow passing params to feathersClient.authenticate()

--- src/auth-module/auth-module.actions.ts
+++ src/auth-module/auth-module.actions.ts
@@ -9,15 +9,23 @@ import { getNameFromPath } from '../utils'
 
 export default function makeAuthActions(feathersClient) {
   return {
-    authenticate(store, data) {
+    authenticate(store, dataOrArray) {
       const { commit, state, dispatch } = store
+      let data
+      let params
+      if (Array.isArray(dataOrArray)) {
+        data = dataOrArray[0]
+        params = dataOrArray[1]
+      } else {
+        data = dataOrArray
+      }
 
       commit('setAuthenticatePending')
       if (state.errorOnAuthenticate) {
         commit('clearAuthenticateError')
       }
       return feathersClient
-        .authenticate(data)
+        .authenticate(data, params)
         .then(response => {
           return dispatch('responseHandler', response)
         })
