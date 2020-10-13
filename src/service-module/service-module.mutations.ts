5f7ac4094b116b26b03df7325f45c1e52fb25eb9
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-10-13 18:13:59 +0200

    chore: change mutations for copy-handling - removeItem removes copy - removeItems removes copy - clearAll also clears copiesById - make clearCopy-mutation cleaner

--- src/service-module/service-module.mutations.ts
+++ src/service-module/service-module.mutations.ts
@@ -173,9 +173,17 @@ export default function makeServiceMutations() {
       const isIdOk = idToBeRemoved !== null && idToBeRemoved !== undefined
       const index = state.ids.findIndex(i => i === idToBeRemoved)
 
+      const Model = _get(models, `[${state.serverAlias}][${state.modelName}]`)
+      const copiesById = state.keepCopiesInStore
+        ? state.copiesById
+        : Model.copiesById
+
       if (isIdOk && index !== null && index !== undefined) {
         Vue.delete(state.ids, index)
         Vue.delete(state.keyedById, idToBeRemoved)
+        if (copiesById.hasOwnProperty(idToBeRemoved)) {
+          Vue.delete(copiesById, idToBeRemoved)
+        }
       }
     },
 
@@ -211,8 +219,17 @@ export default function makeServiceMutations() {
         map[id] = true
         return map
       }, {})
+
+      const Model = _get(models, `[${state.serverAlias}][${state.modelName}]`)
+      const copiesById = state.keepCopiesInStore
+        ? state.copiesById
+        : Model.copiesById
+
       idsToRemove.forEach(id => {
         Vue.delete(state.keyedById, id)
+        if (copiesById.hasOwnProperty(id)) {
+          Vue.delete(copiesById, id)
+        }
       })
 
       // Get indexes to remove from the ids array.
@@ -242,6 +259,18 @@ export default function makeServiceMutations() {
     clearAll(state) {
       state.ids = []
       state.keyedById = {}
+
+      if (state.keepCopiesInStore) {
+        state.copiesById = {}
+      } else {
+        const Model = _get(
+          models,
+          `[${state.serverAlias}].byServicePath[${state.servicePath}]`
+        )
+        Object.keys(Model.copiesById).forEach(k =>
+          Vue.delete(Model.copiesById, k)
+        )
+      }
     },
 
     // Creates a copy of the record with the passed-in id, stores it in copiesById
@@ -322,15 +351,15 @@ export default function makeServiceMutations() {
     // Removes the copy from copiesById
     clearCopy(state, id) {
       const { keepCopiesInStore } = state
-      if (keepCopiesInStore) {
-        Vue.delete(state.copiesById, id)
-      } else {
-        const { serverAlias, servicePath } = state
-        const Model = _get(
-          models,
-          `[${serverAlias}].byServicePath[${servicePath}]`
-        )
-        Vue.delete(Model.copiesById, id)
+      const Model = _get(
+        models,
+        `[${state.serverAlias}].byServicePath[${state.servicePath}]`
+      )
+
+      const copiesById = keepCopiesInStore ? state.copiesById : Model.copiesById
+
+      if (copiesById[id]) {
+        Vue.delete(copiesById, id)
       }
     },
 
