9e33d19f3c46f8a42db64c27e7b65eadd05e1bd1
Marshall Thompson <marshall@creativeideal.net>
2020-12-04 18:18:21 -0700

    Rename mergeWithAccessors to just merge

--- packages/feathers-vuex-vue3/src/utils.vue3.ts
+++ packages/feathers-vuex-vue3/src/utils.vue3.ts
@@ -1,3 +1,3 @@
 // TODO Implement for Vue 3
 export function updateOriginal(original, newData) {}
-export function mergeWithAccessors(original, newData) {}
+export function merge(original, newData) {}
